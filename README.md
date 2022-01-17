7c2ae9dcc186efc515994940ac313db73ab5be65
Keunbae Park <ppotpo@gmail.com>
2022-01-17 18:45:37 -0500

    wip

---
+++ README.md
@@ -0,0 +1,16 @@
+# Show a list of projects
+
+## Usage
+
+### Change `code`
+
+This is a root dirctory. Add additional directories to search for projects
+
+```python
+code = '/home/y/code'
+packages = path.join(code, 'packages')
+```
+
+### Trigger
+
+Trigger this extension with `co`.
---
+++ __init__.py
@@ -0,0 +1,30 @@
+from albert import *
+from os import path
+
+__title__ = "Git clone"
+__version__ = "0.4.0"
+__triggers__ = "clone "
+__authors__ = "pkboom"
+
+icon = "{}/icon.png".format(path.dirname(__file__))
+
+def handleQuery(query):
+    items = []
+
+    if not query.isTriggered or not query.isValid:
+        return
+
+    print(query.string)
+
+    items.append(Item(
+        id='clone',
+        icon=icon,
+        text='git clone ' + query.string,
+        actions=[
+            TermAction(text='git clone', 
+                commandline=['git', 'clone',  query.string], 
+                cwd='/home/y/code')
+        ],
+    ))
+
+    return items
